b78399bf6fb24f31c030e2dba7938a078fecc5fd
Virat jaat <126441859+Anon873@users.noreply.github.com>
2023-07-12 04:51:40 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -2,7 +2,6 @@ import math, time
 from datetime import datetime
 from pytz import timezone
 from config import Config
-from hlep import txt
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 

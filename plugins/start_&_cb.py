30865fb608a1a8af3a4d49190e3992d8623a35ca
Virat jaat <126441859+Anon873@users.noreply.github.com>
2023-07-12 16:47:19 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -28,6 +28,19 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 from helper.database import db
 from config import Config
 
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+    user = message.from_user
+    await db.add_user(client, message)
+    button = InlineKeyboardMarkup([[
+                InlineKeyboardButton("ᴄᴏɴᴛᴀᴄᴛ", callback_data = "apk"),
+                InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data = "close")
+            ]]) 
+       if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
+    else:
+        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+
 
 @Client.on_message((filters.private & filters.command("start")) & filters.user(Config.ADMIN))
 async def start(client, message):

1a4ccc7637b46381d8788d3ac295227b554c0c65
Virat jaat <126441859+Anon873@users.noreply.github.com>
2023-07-12 16:50:52 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -35,8 +35,8 @@ async def start(client, message):
     button = InlineKeyboardMarkup([[
                 InlineKeyboardButton("ᴄᴏɴᴛᴀᴄᴛ", callback_data = "apk"),
                 InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data = "close")
-            ]]) 
-       if Config.START_PIC:
+    ]]) 
+    if Config.START_PIC:
         await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
     else:
         await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
